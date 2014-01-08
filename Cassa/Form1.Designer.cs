a8d7f47e9d96bd9983702adb9de61dbcdf2b02a6
xRoyal <cyborg5095@gmail.com>
2014-01-08 01:14:20 +0100

    Cassa
    Cassa c#

---
+++ Cassa/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace Cassa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Desctxt = new System.Windows.Forms.TextBox();
+            this.przkg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prodottobtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.descacq = new System.Windows.Forms.ListBox();
+            this.catakg = new System.Windows.Forms.ListBox();
+            this.quantCB = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.prodottobtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.przkg);
+            this.panel1.Controls.Add(this.Desctxt);
+            this.panel1.Location = new System.Drawing.Point(37, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 169);
+            this.panel1.TabIndex = 0;
+            // 
+            // Desctxt
+            // 
+            this.Desctxt.Location = new System.Drawing.Point(32, 65);
+            this.Desctxt.Name = "Desctxt";
+            this.Desctxt.Size = new System.Drawing.Size(137, 20);
+            this.Desctxt.TabIndex = 0;
+            // 
+            // przkg
+            // 
+            this.przkg.Location = new System.Drawing.Point(209, 65);
+            this.przkg.Name = "przkg";
+            this.przkg.Size = new System.Drawing.Size(55, 20);
+            this.przkg.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(81, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NUOVO PRODOTTO";
+            // 
+            // prodottobtn
+            // 
+            this.prodottobtn.Location = new System.Drawing.Point(97, 113);
+            this.prodottobtn.Name = "prodottobtn";
+            this.prodottobtn.Size = new System.Drawing.Size(103, 38);
+            this.prodottobtn.TabIndex = 3;
+            this.prodottobtn.Text = "Inserisci Nella Lista";
+            this.prodottobtn.UseVisualStyleBackColor = true;
+            this.prodottobtn.Click += new System.EventHandler(this.prodottobtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Descrizione:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Prezzo al KG:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.quantCB);
+            this.panel2.Controls.Add(this.catakg);
+            this.panel2.Controls.Add(this.descacq);
+            this.panel2.Location = new System.Drawing.Point(60, 258);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(241, 271);
+            this.panel2.TabIndex = 1;
+            // 
+            // descacq
+            // 
+            this.descacq.Enabled = false;
+            this.descacq.FormattingEnabled = true;
+            this.descacq.Location = new System.Drawing.Point(26, 46);
+            this.descacq.Name = "descacq";
+            this.descacq.Size = new System.Drawing.Size(89, 147);
+            this.descacq.TabIndex = 0;
+            this.descacq.SelectedIndexChanged += new System.EventHandler(this.descacq_SelectedIndexChanged);
+            // 
+            // catakg
+            // 
+            this.catakg.Enabled = false;
+            this.catakg.FormattingEnabled = true;
+            this.catakg.Location = new System.Drawing.Point(160, 46);
+            this.catakg.Name = "catakg";
+            this.catakg.Size = new System.Drawing.Size(40, 147);
+            this.catakg.TabIndex = 1;
+            this.catakg.SelectedIndexChanged += new System.EventHandler(this.catakg_SelectedIndexChanged);
+            // 
+            // quantCB
+            // 
+            this.quantCB.Enabled = false;
+            this.quantCB.FormattingEnabled = true;
+            this.quantCB.Location = new System.Drawing.Point(26, 229);
+            this.quantCB.Name = "quantCB";
+            this.quantCB.Size = new System.Drawing.Size(47, 21);
+            this.quantCB.TabIndex = 2;
+            this.quantCB.SelectedIndexChanged += new System.EventHandler(this.quantCB_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(124, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Aggiungi al Carrello";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.listBox4);
+            this.panel3.Controls.Add(this.listBox3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(367, 46);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(261, 480);
+            this.panel3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(105, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "CASSA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(40, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CATALOGO ED ACQUISTO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(699, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(712, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Svuota Tutto";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(868, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Nuovo Scontrino";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(792, 419);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 45);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Esci";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Enabled = false;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(36, 74);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(95, 277);
+            this.listBox3.TabIndex = 1;
+            // 
+            // listBox4
+            // 
+            this.listBox4.Enabled = false;
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(167, 74);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(50, 277);
+            this.listBox4.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(86, 417);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 420);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "€";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Descrizione:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(141, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Prezzo al KG:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(44, 55);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Prodotti Acquistati:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(145, 55);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Importi da Pagare:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(110, 392);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 13);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "TOTALE";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(26, 211);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Quantità:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 563);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Cassa";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button prodottobtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox przkg;
+        private System.Windows.Forms.TextBox Desctxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox quantCB;
+        private System.Windows.Forms.ListBox catakg;
+        private System.Windows.Forms.ListBox descacq;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Cassa/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cassa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 15; i++)
+            {
+                quantCB.Items.Add(i);
+            }
+
+            quantCB.SelectedIndex = 0;
+        }
+
+        private void prodottobtn_Click(object sender, EventArgs e)
+        {
+            if ((Desctxt.Text == "") || (przkg.Text == ""))
+            {
+                MessageBox.Show("Inserisci un prodotto e il rispettivo prezzo!", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                double x = 0;
+
+                if (!double.TryParse(przkg.Text, out x))
+                    MessageBox.Show("Devi Specificare un numero come prezzo!", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    if (!descacq.Enabled)
+                    {
+                        descacq.Enabled = true;
+                        catakg.Enabled = true;
+                        quantCB.Enabled = true;
+                        button1.Enabled = true;
+                    }
+                    descacq.Items.Add(Desctxt.Text.ToUpper());
+                    catakg.Items.Add(przkg.Text);
+                    Desctxt.Clear();
+                    przkg.Clear();
+                    Desctxt.Focus();
+                }
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quantCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            Desctxt.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (quantCB.Text == "")
+            {
+                MessageBox.Show("Seleziona una Quantità!", "Avviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (descacq.SelectedIndex < 0)
+                    MessageBox.Show("Scegli prima un Prodotto!", "Avviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    double prezzo = Convert.ToDouble(catakg.Items[catakg.SelectedIndex]);
+                    double qta = Convert.ToDouble(quantCB.Text);
+                    listBox3.Items.Add(Convert.ToString(qta) + "kg di " + descacq.Items[descacq.SelectedIndex]);
+                    listBox4.Items.Add(prezzo * qta);
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) + prezzo * qta);
+                    button2.Enabled = true;
+
+                }
+            }
+        }
+
+        private void descacq_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            catakg.SelectedIndex = descacq.SelectedIndex;
+        }
+
+        private void catakg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            descacq.SelectedIndex = catakg.SelectedIndex;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i<Controls.Count; i++)
+                if (Controls[i] is Panel)
+                    for (int c = 0; c<Controls[i].Controls.Count; c++)
+                        if (Controls[i].Controls[c] is ListBox)
+                            ((ListBox)Controls[i].Controls[c]).Items.Clear();
+                        else
+                            if (Controls[i].Controls[c] is TextBox)
+                                ((TextBox)Controls[i].Controls[c]).Clear();
+            textBox1.Text = "0";
+            Desctxt.Focus();
+            button2.Enabled = false;
+                                              
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Controls.Count; i++)
+                if (Controls[i] is Panel)
+                    for (int c = 0; c < Controls[i].Controls.Count; c++)
+                        if (Controls[i].Controls[c] is ListBox)
+                            ((ListBox)Controls[i].Controls[c]).Items.Clear();
+                        else
+                            if (Controls[i].Controls[c] is TextBox)
+                                ((TextBox)Controls[i].Controls[c]).Clear();
+            textBox1.Text = "0";
+            Desctxt.Focus();
+        }
+    }
+}
